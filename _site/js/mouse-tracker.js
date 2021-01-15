10d63516fc790413494dcb5eac477c6feb9bd0b4
Lulu <dowlu@hotmail.com>
2021-01-15 09:26:32 +0100

    try not ignore site folder

---
+++ _site/js/mouse-tracker.js
@@ -0,0 +1,55 @@
+/* Animated cursor
+ * source: https://css-tricks.com/using-css-cursors/
+ */
+
+(function() {
+
+    var follower = document.getElementById('follower');
+
+    var mouseX = (function(_this) {
+        return function(event) {
+            return event.clientX;
+        };
+    })(this);
+
+    var mouseY = (function(_this) {
+        return function(event) {
+            return event.clientY;
+        };
+    })(this);
+
+    var positionElement = (function(_this) {
+        return function(event) {
+            var mouse;
+            mouse = {
+                x: mouseX(event),
+                y: mouseY(event)
+            };
+            follower.style.top = mouse.y + 'px';
+            return follower.style.left = mouse.x + 'px';
+        };
+    })(this);
+
+    var timer = false;
+    var init;
+
+    window.onmousemove = init = (function(_this) {
+        return function(event) {
+            var _event;
+            _event = event;
+            return timer = setTimeout(function() {
+                return positionElement(_event);
+            }, 1);
+        };
+    })(this);
+
+}).call(this);
+
+var cursorShow = function (cursorType) {
+    //setTimeout(function () {
+        $('#cursor').addClass(cursorType);
+    //},300);    
+};
+var cursorHide = function (cursorType) {
+    $('#cursor').removeClass(cursorType);
+};
