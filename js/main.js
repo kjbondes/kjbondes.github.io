1614097147f924ce5062410cdf2953e92439d900
Lulu <dowlu@hotmail.com>
2021-01-16 09:17:09 +0100

    detect user lang

--- js/main.js
+++ js/main.js
@@ -1,19 +1,9 @@
-/* modal */
-var objectShow = (function(objectID){ 
-  $(objectID).show(); 
-})
-var objectHide = (function(objectID){ 
-  $(objectID).hide(); 
-})
-
-/* scroll down */
-var scrollDownTo = (function(objectID){
-  var width = $(window).width();
-  if (width >= 992) { // $screenM
-    $('html,body').animate({ scrollTop : $(objectID).offset().top -75 }, 1000);
-  } else if ( width >= 576 ) { // $screenXS
-    $('html,body').animate({ scrollTop : $(objectID).offset().top -60 }, 1000);
-  } else { 
-    $('html,body').animate({ scrollTop : $(objectID).offset().top -50 }, 1000);
+$( document ).ready(function(){
+  var userLang = navigator.language || navigator.userLanguage;
+  if (userLang == "de") {
+    window.location.href = "https://kjbondes.github.io"
+  }
+  else {
+    window.location.href = "https://kjbondes.github.io/en"
   }
 });
