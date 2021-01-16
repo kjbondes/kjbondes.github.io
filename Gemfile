c9d9a42062bb06a6f873b4e17b76e14c6bf141d7
Lulu <dowlu@hotmail.com>
2021-01-16 09:54:37 +0100

    detect lang only when loading homepage

--- Gemfile
+++
@@ -1,5 +0,0 @@
-gem 'jekyll-multiple-languages-plugin', '~> 1.7'
-gem 'webrick'
-
-source 'https://rubygems.org'
-gem 'github-pages'
--- _site/js/main.js
+++ _site/js/main.js
@@ -1,9 +1,45 @@
 $( document ).ready(function(){
   var userLang = navigator.language || navigator.userLanguage;
-  if (userLang == "de") {
-    window.location.href = "kjbondes.github.io"
+
+  var hostname = window.location.hostname;
+  var referrer = document.referrer;
+
+  var landingPage = !referrer || referrer.indexOf(hostname) == -1;
+
+  if (landingPage && userLang !== "de") {
+    window.location = '/en/';
   }
-  else {
-    window.location.href = "kjbondes.github.io/en"
+})
+
+/*
+$( document ).ready(function(){
+  var [lang, locale] = (((navigator.userLanguage || navigator.language).replace('-', '_')).toLowerCase()).split('_');
+
+  function getBestSuitableSupportedLang(lang, locale, supported) {
+    // Exclude first element, default language
+    var supported_lang = supported.shift();
+    
+    if (supported.includes(lang + "-" + locale)) {
+      supported_lang = lang + "-" + locale;
+    } else if (supported.includes(lang)) {
+      supported_lang = lang;
+    }
+
+    return supported_lang;
   }
-});
+
+  var supported_languages = ["en", "de"];
+  var current_lang = '';
+  var suitable_lang = getBestSuitableSupportedLang(lang, locale, supported_languages);
+
+  var hostname = window.location.hostname;
+  var referrer = document.referrer;
+
+  var landingPage = !referrer || referrer.indexOf(hostname) == -1;
+
+  if (landingPage && (current_lang !== suitable_lang)) {
+    window.location = '/' + suitable_lang + '/';
+  }
+
+})
+*/
