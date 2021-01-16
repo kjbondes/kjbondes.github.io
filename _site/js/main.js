19b0d1420ffc350191c6976ea1410f300115f8cc
Lulu <dowlu@hotmail.com>
2021-01-16 10:03:01 +0100

    try window.location

--- _site/js/main.js
+++ _site/js/main.js
@@ -7,7 +7,8 @@ $( document ).ready(function(){
   var landingPage = !referrer || referrer.indexOf(hostname) == -1;
 
   if (landingPage && userLang !== "de") {
-    window.location.href = 'https://kjbondes.github.io/en';
+    window.location = '/en';
+    //window.location.href = 'https://kjbondes.github.io/en';
   }
 })
 
