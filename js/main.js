e2f4b7d6e2ef6a4db04aa3872e6ea41c46caea7a
Lulu <dowlu@hotmail.com>
2021-01-16 09:28:40 +0100

    detect lang

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $( document ).ready(function(){
   var userLang = navigator.language || navigator.userLanguage;
   if (userLang == "de") {
-    window.location.href = "https://kjbondes.github.io"
+    window.location.href = "kjbondes.github.io"
   }
   else {
-    window.location.href = "https://kjbondes.github.io/en"
+    window.location.href = "kjbondes.github.io/en"
   }
 });
