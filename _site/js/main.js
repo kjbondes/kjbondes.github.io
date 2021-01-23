e7be52bc204c165b59fe681fbe203eacc110a792
Lulu <dowlu@hotmail.com>
2021-01-23 21:24:25 +0100

    Site Update

--- _site/js/main.js
+++ _site/js/main.js
@@ -8,39 +8,5 @@ $( document ).ready(function(){
 
   if (landingPage && userLang !== "de") {
     window.location = '/en';
-    //window.location.href = 'https://kjbondes.github.io/en';
   }
 })
-
-/*
-$( document ).ready(function(){
-  var [lang, locale] = (((navigator.userLanguage || navigator.language).replace('-', '_')).toLowerCase()).split('_');
-
-  function getBestSuitableSupportedLang(lang, locale, supported) {
-    // Exclude first element, default language
-    var supported_lang = supported.shift();
-    
-    if (supported.includes(lang + "-" + locale)) {
-      supported_lang = lang + "-" + locale;
-    } else if (supported.includes(lang)) {
-      supported_lang = lang;
-    }
-
-    return supported_lang;
-  }
-
-  var supported_languages = ["en", "de"];
-  var current_lang = '';
-  var suitable_lang = getBestSuitableSupportedLang(lang, locale, supported_languages);
-
-  var hostname = window.location.hostname;
-  var referrer = document.referrer;
-
-  var landingPage = !referrer || referrer.indexOf(hostname) == -1;
-
-  if (landingPage && (current_lang !== suitable_lang)) {
-    window.location = '/' + suitable_lang + '/';
-  }
-
-})
-*/
