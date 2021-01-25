4ab4987922a334cb8b8f5a6626a0c1797cd609ba
Lulu <dowlu@hotmail.com>
2021-01-25 18:28:21 +0100

    change lang folder

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $( document ).ready(function(){
   var hostname = window.location.hostname;
   var referrer = document.referrer;
 
+  // check if reloading the page
   var landingPage = !referrer || referrer.indexOf(hostname) == -1;
 
   if (landingPage && userLang !== "de") {
